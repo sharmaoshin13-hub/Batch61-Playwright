f1a873f7cab3e0624d313272df7c8f675beb647c
sharmaoshin13-hub <sharmaoshin13@gmail.com>
2026-04-03 17:02:21 +0530

    Implement prime number check functionality

--- Assignments/Primenumberornot.js
+++ Assignments/Primenumberornot.js
@@ -1,1 +1,19 @@
+// Take input from the user
+const num = parseInt(prompt("Enter a number: "), 10);
+
+// Function to check prime
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+// Output in required format
+if (isPrime(num)) {
+  console.log(`provided number ${num} is prime`);
+} else {
+  console.log(`provided number ${num} is not-prime`);
+}
 
