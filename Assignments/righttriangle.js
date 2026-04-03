3128a6d61cb49802bb671024cc0044082bfd6730
sharmaoshin13-hub <sharmaoshin13@gmail.com>
2026-04-03 17:58:55 +0530

    Update righttriangle.js

--- Assignments/righttriangle.js
+++ Assignments/righttriangle.js
@@ -11,7 +11,7 @@ rl.question("Enter number of rows: ", function(rows) {
   for (let i = 1; i <= rows; i++) {
     let pattern = "";
     for (let j = 1; j <= i; j++) {
-      pattern += "* ";
+      pattern += pattern += String.fromCharCode(65 + j - 1) + " ";
     }
     console.log(pattern);
   }
