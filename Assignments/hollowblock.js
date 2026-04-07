e16412bcaedeefc7865a5e72e272da8f89b8aefa
sharmaoshin13-hub <sharmaoshin13@gmail.com>
2026-04-07 18:21:36 +0530

    Update hollowblock.js

--- Assignments/hollowblock.js
+++ Assignments/hollowblock.js
@@ -1,1 +1,16 @@
 
+function hollowBlockAlphabets(n) {
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= n; j++) {
+      if (i === 1 || i === n || j === 1 || j === n) {
+        row += String.fromCharCode(65 + i - 1) + " "; // A=65
+      } else {
+        row += "  ";
+      }
+    }
+    console.log(row);
+  }
+}
+
+hollowBlockAlphabets(5);
