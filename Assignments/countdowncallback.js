35e7320879cf3afc99000a371c34cad452daf538
sharmaoshin13-hub <sharmaoshin13@gmail.com>
2026-04-07 12:11:46 +0530

    Update countdowncallback.js

--- Assignments/countdowncallback.js
+++ Assignments/countdowncallback.js
@@ -1,1 +1,20 @@
 
+function countdown(start, callback) {
+  if (start === 0) {
+    // When countdown finishes, call the callback
+    callback();
+    return;
+  }
+
+  console.log(start);
+
+  // Wait 1 second, then call countdown again with start - 1
+  setTimeout(() => {
+    countdown(start - 1, callback);
+  }, 1000);
+}
+
+// Example usage
+countdown(5, () => {
+  console.log("Go!");
+});
